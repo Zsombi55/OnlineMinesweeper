94679a66c60b51503105cf208102f04aa454de9d
Zsombi55 <47913849+Zsombi55@users.noreply.github.com>
2019-12-10 18:00:15 +0200

    Rewrite, 2nd try, with less help.

--- functions.js
+++ functions.js
@@ -1,141 +1,105 @@
-var box = document.getElementById("box");
-var testMineView = true;    // For testing.
+var grid = document.getElementById("box");
+var row, col, cell;
+var testMode = true; //For testing. Turn this "true" to see where the mines are.
 
-makeBox();
+var xSize = 10; // horizontal (x axis) cells
+var ySize = 10; // vertical (y axis) cells
+var mCount = 20; // Number of mines.
 
-// Draw the grid like box. Size 10^2 for now.
-function makeBox() {
-    box.innerHTML = ""; // set a blank string as value to the ID-d table element.
-
-    for (var i = 0; i < 10; i++) {
-        row = box.insertRow(i); // insertRow & -Cell are JS functions.
-        for (var j = 0; j < 10; j++) {
-            panel = row.insertCell(j);
-            panel.onclick = function () {   clickPanel(this);    };
-
-            var mine = document.createAttribute("data-mine");
-            mine.value = "false";
-            panel.setAttributeNode(mine);
-        }
-    }
-    layMines();
-    document.getElementById("winLose").innerText = "";
-}
-
-// Add mines randomly.
-function layMines() {
-    // The comparison value is the nr. of mines.
-    for (var i = 0; i < 15; i++) {
-        var row = Math.floor(Math.random() * 10);
-        var column = Math.floor(Math.random() * 10);
-        var panel = box.rows[row].cells[column];
-
-        panel.setAttribute("data-mine", "true");
-
-        // Mark mines if the condition value is "true".
-        if (testMineView) { panel.innerHTML = "X"; }
-    }
-}
-
-// Reveal all mines by coloring the panel red.
-function showMines() {
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            var panel = box.rows[i].cells[j];
-
-            if (panel.getAttribute("data-mine") == "true") {
-                panel.className = "mine";
-            }
-        }
-    }
-}
-
-function levelStatus() {
-    var isDone = true;  // Is an end (win, loss) reached?
-
-    for (var i = 0; i < 10; i++) {
-        for (var j = 0; j < 10; j++) {
-            if (box.rows[i].cells[j].getAttribute("data-mine" == "false") &&
-                box.rows[i].cells[j].innerHTML == "") {
-                isDone = false;
-            }
-        }
-    }
-
-    if (isDone) {
-        console.warn("Won!");
-        document.getElementById("winLose").innerText = "You have won!";
-        showMines();
-    }
-}
-
-// Panel clicking event handler.
-function clickPanel(panel) {
-    if (panel.getAttribute("data-mine") == "true") {
-        showMines();
-        console.warn("Lost!");
-        document.getElementById("winLose").innerText = "You have lost!";
-    } else {
-        panel.className = "clicked";
-        countMines(panel);
-        
-        // Count and show the nr. of all adjacent mines.
-        var amount = 0;
-        var panelRow = panel.parentNode.rowIndex;   // row- & cellIndex are JS variables.
-        var panelColumn = panel.cellIndex;
-
-        for (var i = Math.max(panelRow - 1, 0); i <= Math.min(panelRow + 1, 9); i++) {
-            for (var j = Math.max(panelColumn - 1, 0); j <= Math.min(panelColumn + 1, 9); j++) {
-                if (box.rows[i].cells[j].getAttribute("data-mine") == "true") {
-                    amount++;
-                }
-            }
-        }
-        panel.innerHTML = amount;
-        if (amount == 0) {
-            // Show all adjacent safe panels.
-            for (var i = Math.max(panelRow - 1, 0); i <= Math.min(panelRow + 1, 9); i++) {
-                for (var j = Math.max(panelColumn - 1, 0);  j <= Math.min(panelColumn + 1, 9); j++) {
-                    // This is a Recursive call.
-                    if (box.rows[i].cells[j].innerHTML == "") {
-                        clickPanel(box.rows[i].cells[j]);
-                    }
-                }
-            }
-        }
-        levelStatus();
-    }
-}
-
-/*
-// Count and show the nr. of all adjacent mines.
-function countMines(panel) {
-    var amount = 0;
-    var panelRow = panel.parentNode.rowIndex;   // row- & cellIndex are JS variables.
-    var panelColumn = panel.cellIndex;
-
-    for (var i = Math.max(panelRow - 1, 0); i <= Math.min(panelRow + 1, 9); i++) {
-        for (var j = Math.max(panelColumn - 1, 0); j <= Math.min(panelColumn + 1, 9); j++) {
-            if (box.rows[i].cells[j].getAttribute("data-mine") == "true") {
-                amount++;
-            }
-        }
-    }
-    panel.innerHTML = amount;
-    if (amount == 0) {
-        showSafe(panelRow, panelColumn);
-    }
-    levelStatus();
-}
-
-// Show all adjacent safe panels.
-function showSafe(panelRow, panelColumn) {
-    for (var i = Math.max(panelRow - 1, 0); i <= Math.min(panelRow + 1, 9); i++) {
-        for (var j = Math.max(panelColumn - 1, 0);  j <= Math.min(panelColumn + 1, 9); j++) {
-            // This is a Recursive call.
-            if (box.rows[i].cells[j].innerHTML == "") {
-                clickPanel(box.rows[i].cells[j]);
-            }
-        }
-    }
+/* function Cell() { // Cell object with relevant attributes.
+    this.mine = true; // Does the cell have a dot?
+    this.revealed = true; // Is the cell revealed?
 } */
+
+newScene(); // Initialize & show the grid when the page loads.
+
+function newScene(){ // Re-initialize & reset the grid when the "New" button is pressed.
+    console.log("new");
+    makeGrid(xSize, ySize); // --- OK ---
+    //makeMines(cell);
+    addMines(); // --- OK ---
+    mapGridMines(); // --- BAD ---
+}
+
+// Make a generalized 2 dimensional Array with the specified sizes.
+function makeGrid(xSize, ySize){
+    grid.innerHTML = ""; // Set a blank string as value to the ID-d table element.
+
+    for(var x = 0; x < xSize; x++){
+        row = grid.insertRow(x); // Add /make a row in the grid /table.
+        for(var y = 0; y < ySize; y++){
+            cell = row.insertCell(y); // Make /designate cells /columns in the current row of the grid /table.
+            cell.onclick = function () { clickCell(this); }; // Each cell told what to do if "this" cell is clicked.
+        }
+    }
+}
+
+function clickCell(_cell) { // Patameter ignored ("_") until function made useful.
+    console.log("cell clicked");
+}
+
+function addMines(){ //Add mines randomly; set cell attributes accordingly.
+    console.log(cell);
+    // Make the mines.
+    var mine = document.createAttribute("data-mine"); // make custom attribute.
+    mine.value = "false"; // Add a value to this attribute.
+    cell.setAttributeNode(mine); // Attach this attribute to the cell object.
+
+    for (var x = 0; x < mCount; x++) {
+        row = Math.floor(Math.random() * 10);
+        col = Math.floor(Math.random() * 10);
+        cell = grid.rows[row].cells[col];
+        cell.setAttribute("data-mine", "true");
+        if (testMode) cell.innerHTML = "&#9788;"; // Show the planted mines; for testing ?
+    }
+}
+
+function mapGridMines() {
+    var mineNumber = document.createAttribute("data-number");
+    mineNumber.value = "false";
+    cell.setAttributeNode(mineNumber);
+    
+    for (var x = 0; x < xSize; x++) {
+        for (var y = 0; y < ySize; y++) {
+            /* console.log(`Top.... left: ${x-1} , ${y-1} ; middle: ${x-1} , ${y} ; right: ${x-1} , ${y+1} \n` +
+                        `Center. left: ${x} , ${y-1} ; middle: ${x} , ${y} ; right: ${x} , ${y+1} \n` +
+                        `Bottom. left: ${x+1} , ${y-1} ; middle: ${x+1} , ${y} ; right: ${x+1} , ${y+1}`); */
+            console.log(cell);
+            //if(cell.hasAttribute("data-mine", "true")) {
+            if(!cell.hasAttribute("data-mine")) {
+                console.log("Empty, checking..");
+                countAround();
+            }
+            else { console.log("Has mine."); }
+        }
+    }
+}
+                    //      -------- see v0 for this part, use "grid." to check what cells have what attributes ! -----
+
+function countAround() { // Go through the grid; count mine aroung each cell.
+    var counter = 0;
+    
+    for (var x = -1; x < 2; x++) {
+        for (var y = -1; y < 2; y++) {
+            console.log(`Checking neighbor: ${x} , ${y}`);
+            
+            // --check IF the current cell [x,y] has the Attribute of "data-mine" or not;
+            // --if yes, then increment the "counter" by 1.
+            if(cell.hasAttribute("data-mine") == "true") {
+                counter++;
+            }
+        }
+    }
+    // --IF the "counter" is larger than 0, add that to the table cell as value and mark it as "numbered",
+    // --if not, then mark the cell "empty".
+    if(counter > 0) {
+        cell.innerHTML = counter;
+        cell.removeAttribute("data-mine");
+        cell.setAttribute("data-number", "true");
+    }
+    else {cell.innerHTML = "0";}
+    console.log("Counter: " + counter);
+}
+
+// white flag &#9872;
+// black flag &#9873;
